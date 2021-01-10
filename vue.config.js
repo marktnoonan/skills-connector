c93d6a1e11a3ab29ded308a1ddc48138642ccc6f
Mark Noonan <markthomasnoonan@gmail.com>
2021-01-10 17:11:42 -0500

    add pre-render

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: [
+        '/'
+      ],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true
+    }
+  }
 }
